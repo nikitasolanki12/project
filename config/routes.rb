5b182e831ef2d006d8303cb064979fdbbb396316
Nikita Solanki <Nikita@techwinners.in>
2022-12-03 12:55:33 +0530

    use-devise

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users, controllers: {
+           :sessions => "users/sessions",
+           :registrations => "users/registrations"}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "home#index"
+end
---
+++ db/migrate/20221126093721_addname_to_users.rb
@@ -0,0 +1,9 @@
+class AddnameToUsers < ActiveRecord::Migration[7.0]
+  def change
+     add_column :users, :first_name, :string
+     add_column :users, :last_name, :string
+     add_column :users, :date_of_birth, :date
+     add_column :users, :city, :string
+     add_column :users, :gender, :string
+  end
+end
